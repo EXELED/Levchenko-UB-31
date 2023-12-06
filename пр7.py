d300f5c0bf058ac2c49ff0df443c7e69e21e68e7
EXELED <147983035+EXELED@users.noreply.github.com>
2023-12-06 08:05:20 +0300

    Add files via upload

---
+++ пр7.py
@@ -0,0 +1,11 @@
+from math import *
+def t2(x, y):
+    return sqrt(x**2+y**2) # квадрат гипотенузы
+if t2(10,8)>t2(11,4):
+    print('1-ая больше')
+else:
+    print('1-ая меньше')
+
+a=['ш','и','г','у','н','о','р','а']
+a.sort()
+print(a)
